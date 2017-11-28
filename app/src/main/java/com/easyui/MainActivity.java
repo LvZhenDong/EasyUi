eddd86249300ba49ac0625234e1d2b2afc27717f
lvzhendong <lvzhendong1993@gmail.com>
2017-11-28 10:34:26 +0800

    SpannableStringUtils

--- app/src/main/java/com/easyui/MainActivity.java
+++ app/src/main/java/com/easyui/MainActivity.java
@@ -1,12 +1,20 @@
 package com.easyui;
 
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.emiya.ui.activity.BaseActivity;
+import com.emiya.util.SpannableStringUtils;
+
+import butterknife.BindView;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.tv_spannable)
+    TextView mTvSpannable;
 
     @Override
     public int returnLayoutID() {
@@ -32,5 +40,12 @@ public class MainActivity extends BaseActivity {
                 Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
             }
         });
+
+        SpannableStringBuilder spannableStringBuilder = SpannableStringUtils.getBuilder("一二34五")
+                .setForegroundColor(getResources().getColor(R.color.red))
+                .append("text2").setForegroundColor(getResources().getColor(R.color.colorPrimary))
+                .create();
+        mTvSpannable.setText(spannableStringBuilder);
     }
+
 }
