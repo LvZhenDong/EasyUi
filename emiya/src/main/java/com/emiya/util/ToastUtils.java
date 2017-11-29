6999eb3c68a5f6cfd88a9b8361e262b95b5e58c4
lvzhendong <lvzhendong1993@gmail.com>
2017-11-29 15:16:35 +0800

    ToastUtil

---
+++ emiya/src/main/java/com/emiya/util/ToastUtils.java
@@ -0,0 +1,40 @@
+package com.emiya.util;
+
+import android.content.Context;
+import android.os.Looper;
+import android.support.annotation.StringRes;
+import android.widget.Toast;
+
+public class ToastUtils {
+
+    private static Toast toast = null;
+
+    public static void show(Context context, String text) {
+        try {
+            if (toast != null) {
+                toast.setText(text);
+            } else {
+                toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+            }
+            toast.show();
+        } catch (Exception e) {//子线程中Toast异常情况处理
+            Looper.prepare();
+            Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+            Looper.loop();
+        }
+    }
+
+    public static void show(Context context,@StringRes int resId){
+        show(context,context.getString(resId));
+    }
+
+    public static void showInOtherThread(final Context context, final String text) {
+        ThreadUtil.getMainThreadHandler().post(new Runnable() {
+            @Override
+            public void run() {
+                toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+}
