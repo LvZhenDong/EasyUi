4a306c2f4135bc7575d877bd55c084e433bb8656
lvzhendong <lvzhendong1993@gmail.com>
2017-11-29 14:56:09 +0800

    FlycoTabLayout

--- app/src/main/java/com/easyui/MainActivity.java
+++ app/src/main/java/com/easyui/MainActivity.java
@@ -7,14 +7,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.emiya.ui.activity.BaseActivity;
+import com.emiya.ui.widgets.TabEntity;
 import com.emiya.util.SpannableStringUtils;
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
 
     @BindView(R.id.tv_spannable)
     TextView mTvSpannable;
+    @BindView(R.id.tl1)
+    CommonTabLayout mTl1;
+    @BindView(R.id.tl2)
+    CommonTabLayout mTl2;
 
     @Override
     public int returnLayoutID() {
@@ -46,6 +57,36 @@ public class MainActivity extends BaseActivity {
                 .append("text2").setForegroundColor(getResources().getColor(R.color.colorPrimary))
                 .create();
         mTvSpannable.setText(spannableStringBuilder);
+
+        ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+        mTabEntities.add(new TabEntity("使用说明"));
+        mTabEntities.add(new TabEntity("常见问题"));
+        mTabEntities.add(new TabEntity("配件耗材"));
+        mTl1.setTabData(mTabEntities);
+        mTl1.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+
+            }
+
+            @Override
+            public void onTabReselect(int position) {
+
+            }
+        });
+
+        ArrayList<CustomTabEntity> mTabEntities2 = new ArrayList<>();
+        mTabEntities2.add(new TabEntity("首页", R.drawable.bg_home_home_rb_checked, R.drawable.bg_home_home_rb));
+        mTabEntities2.add(new TabEntity("视频区", R.drawable.bg_home_parts_rb_checked, R.drawable.bg_home_parts_rb));
+        mTabEntities2.add(new TabEntity("服务支持", R.drawable.bg_home_feedback_rb_checked, R.drawable.bg_home_feedback_rb));
+        mTabEntities2.add(new TabEntity("我的", R.drawable.bg_home_my_rb_checked, R.drawable.bg_home_my_rb));
+        mTl2.setTabData(mTabEntities2);
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
 }
